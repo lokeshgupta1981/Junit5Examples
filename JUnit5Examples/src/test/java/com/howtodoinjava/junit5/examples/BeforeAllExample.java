3e42f4232a0b479048442738fc09cde9dae75be0
Lokesh Gupta <howtodoinjava@gmail.com>
2019-10-05 15:51:34 +0530

    Create BeforeAllExample.java

---
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/BeforeAllExample.java
@@ -0,0 +1,28 @@
+package com.howtodoinjava.junit5.examples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class BeforeAllExample {
+
+	@Test
+	@DisplayName("Add operation test")
+	@RepeatedTest(5)
+	void addNumber(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+		System.out.println("Running test -> " + repetitionInfo.getCurrentRepetition());
+		Assertions.assertEquals(2, Calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+	
+	@BeforeAll
+	public static void init(){
+		System.out.println("Before All init() method called");
+	}
+}
