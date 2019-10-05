6ce04b45940fe8a78cf0ce57c0a0a8483bc94863
Lokesh Gupta <howtodoinjava@gmail.com>
2019-10-05 19:28:24 +0530

    Update tests

--- JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/packageA/ClassATest.java
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/packageA/ClassATest.java
@@ -1,10 +1,12 @@
 package com.howtodoinjava.junit5.examples.packageA;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class ClassATest 
 {
+	@Tag("production")
 	@Test
 	@DisplayName("testCaseA inside ClassATest inside packageA")
 	public void testCaseA() {
--- JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/suites/JUnit5TestSuiteExample.java
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/suites/JUnit5TestSuiteExample.java
@@ -1,5 +1,16 @@
 package com.howtodoinjava.junit5.examples.suites;
 
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"com.howtodoinjava.junit5.examples.packageA"
+				,"com.howtodoinjava.junit5.examples.packageB"})
+@IncludeClassNamePatterns({"^.*ATests?$"})
+@IncludeTags("production")
 public class JUnit5TestSuiteExample {
 
 }
