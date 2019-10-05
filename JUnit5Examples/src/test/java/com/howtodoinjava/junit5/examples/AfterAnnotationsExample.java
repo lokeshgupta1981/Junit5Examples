2f09ed6cc30c6b89b9b023f28d17eaa0e7b7c8a3
Lokesh Gupta <howtodoinjava@gmail.com>
2019-10-05 16:09:25 +0530

    Create AfterAnnotationsExample.java

---
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/AfterAnnotationsExample.java
@@ -0,0 +1,36 @@
+package com.howtodoinjava.junit5.examples;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class AfterAnnotationsExample {
+
+	@Test
+	@DisplayName("Add operation test")
+	@RepeatedTest(5)
+	void addNumber(TestInfo testInfo, RepetitionInfo repetitionInfo) 
+	{
+		System.out.println("Running test -> " + repetitionInfo.getCurrentRepetition());
+		Assertions.assertEquals(2, Calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+	
+	@AfterAll
+	public static void cleanUp(){
+		System.out.println("After All cleanUp() method called");
+	}
+	
+	@AfterEach
+	public void cleanUpEach(){
+		System.out.println("After Each cleanUpEach() method called");
+	}
+
+}
