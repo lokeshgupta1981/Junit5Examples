3be29d16442f44b016ff01228dffbde53663b45d
Lokesh Gupta <howtodoinjava@gmail.com>
2019-10-05 17:14:41 +0530

    Create ExpectedExceptionExample.java

---
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/ExpectedExceptionExample.java
@@ -0,0 +1,38 @@
+package com.howtodoinjava.junit5.examples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.howtodoinjava.junit5.examples")
+public class ExpectedExceptionExample 
+{
+	@Test
+	void testExpectedException() {
+
+		Assertions.assertThrows(NumberFormatException.class, () -> {
+			Integer.parseInt("One");
+		});
+
+	}
+
+	@Test
+	void testExpectedExceptionWithSameType() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Integer.parseInt("One");
+		});
+	}
+	
+	@Test
+	void testExpectedExceptionFail() {
+	 
+	  Assertions.assertThrows(IllegalArgumentException.class, () -> {
+	    Integer.parseInt("1");
+	  });
+	 
+	}
+}
