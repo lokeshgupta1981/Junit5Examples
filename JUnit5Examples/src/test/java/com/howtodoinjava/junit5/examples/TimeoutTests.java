ba513b493a05c69db1f094293aeee2faff0bdc68
Lokesh Gupta <lokeshgupta1981@gmail.com>
2021-11-11 01:55:09 +0530

    Create TimeoutTests.java

---
+++ JUnit5Examples/src/test/java/com/howtodoinjava/junit5/examples/TimeoutTests.java
@@ -0,0 +1,35 @@
+package com.howtodoinjava.junit5.examples;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+@Timeout(3)
+public class TimeoutTests {
+
+	@Test
+	void testMethodOne() throws InterruptedException {
+	    TimeUnit.SECONDS.sleep(5);
+	}
+	
+	@Test
+	@Timeout(5)
+	void testMethodTwo() throws InterruptedException {
+	    TimeUnit.SECONDS.sleep(4);
+	}
+	
+	@Test
+	void testGetValue() throws InterruptedException {
+	    Assertions.assertTimeout(Duration.ofSeconds(3), () -> {
+            getValue();
+        });
+	}
+	
+	String getValue() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(2);
+		return "";
+	}
+}
